83fddf66706853c2f0213e103e402ce270145cb5
umar <umar.dev500@gmail.com>
2023-03-03 22:22:50 +0700

    reorganize

--- app/repository/order_repo.go
+++ app/repository/order_repo.go
@@ -1,7 +1,6 @@
 package repository
 
 import (
-	"context"
 	"customer-api/domain"
 	"customer-api/pb"
 )
@@ -14,9 +13,4 @@ func NewOrderRepository(order pb.OrderServiceClient) domain.OrderRepository {
 	return &orderRepository{
 		order: order,
 	}
-}
-
-func (o *orderRepository) FindAll(ctx context.Context, req *pb.OrderFindAllRequest) (res *pb.OrderFindAllResponse, err error) {
-	res, err = o.order.FindAll(ctx, req)
-	return
 }
--- app/repository/user_repo.go
+++ app/repository/user_repo.go
@@ -1,7 +1,6 @@
 package repository
 
 import (
-	"context"
 	"customer-api/domain"
 	"customer-api/pb"
 )
@@ -14,10 +13,4 @@ func NewAppRepository(customer pb.CustomerServiceClient) domain.AppRepository {
 	return &appRepository{
 		customer: customer,
 	}
-}
-
-func (a *appRepository) Find(ctx context.Context, userId string) (res *pb.CustomerFindResponse, err error) {
-	res, err = a.customer.Find(ctx, &pb.CustomerFindRequest{CustomerId: userId})
-
-	return
 }
